41efef9d4164fb578a65060bc9da4d74f4dce120
Jack Reed <jack.reed25@bathspa.ac.uk>
2026-01-15 16:10:17 +0000

    Expanded on comments
    I expanded on the comments and changed some of the code such as the pricing and removed a redundency in input handling

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -9,37 +9,41 @@ void handlePayment(double price);
 bool askAnotherPurchase();
 
 int main() {
+    
     while (true) {
         string itemName;
         double price;
 
-        if (selectItem(itemName, price)) {
-            cout << "Thank you for using the vending machine. Goodbye!\n";
+        // this function displays the menu and allows the user to select an item
+
+        if (!selectItem(itemName, price)) {
+            cout << "Thank you for using the vending machine. Goodbye!\n"; // Allows the user leave the program
             break;
         }
-
+        // this function handles the payment process
         handlePayment(price);
-        cout << itemName << " item dispensed!\n";
+        cout << itemName << " item dispensed!\n"; // Lets the user know that their item is being despenced 
 
-        if (askAnotherPurchase()) {
-            cout << "Thank you for using the vending machine. Goodbye!\n";
+        // this function asks the user if they want to make another purchase
+        if (!askAnotherPurchase()) {
+            cout << "Thank you for using the vending machine. Goodbye!\n"; // Allows the user leave the program
             break;
         }
     }
     return 0;
 }
 
-void viewMenu() { 
+void viewMenu() { // Display's the vending machine menu with listed items and their prices
     cout << "\n"; 
     cout << "========= Welcome to the utility vending machine =========\n"; 
     cout << "\n"; 
-    cout << "           1               2               3\n"; 
+    cout << "           1               2               3\n"; // Column headers
     cout << "\n"; 
-    cout << "A     Water (£1.00)   Juice (£1.50)   Cola (£1.25)\n"; 
+    cout << "A     Water (£.99)   Juice (£1.49)   Cola (£1.25)\n"; // Row A items
     cout << "\n"; 
-    cout << "B     Crisps (£1.75)  Gum (£0.75)    Chocolate (£2.00)\n"; 
+    cout << "B     Crisps (£1.75)  Gum (£0.75)    Chocolate (£1.99)\n"; // Row B items
     cout << "\n"; 
-    cout << "C     Mints (£0.80)   Gummies (£1.60) Biscuits (£1.40)\n\n";
+    cout << "C     Mints (£0.79)   Gummies (£1.59) Biscuits (£1.39)\n"; // Row C items
 }
 
 bool selectItem(string &itemName, double &price) {
@@ -47,104 +51,99 @@ bool selectItem(string &itemName, double &price) {
         viewMenu();
 
         string code;
-        cout << "Enter item code or X to exit: ";
+        cout << "Enter item code or X to exit:"; // Asks the user if they want to leave the program
         cin >> code;
 
-        if (code == "X" || code == "x") return false;
-
-        if (code.length() != 2) {
-            cout << "Invalid code.\n";
-            continue;
-        }
+        if (code == "X" || code == "x") return false;   // Makes sure that lowercase x and uppercase X both work to exit the program
 
         char letter = code[0]; // row
         char number = code[1]; // column
 
-        if (letter == 'a') letter = 'A';
-        else if (letter == 'b') letter = 'B';
-        else if (letter == 'c') letter = 'C';
+        if (letter == 'a') letter = 'A'; // Makes sure that lowercase a and uppercase A both work
+        else if (letter == 'b') letter = 'B'; // Makes sure that lowercase b and uppercase B both work
+        else if (letter == 'c') letter = 'C'; // Makes sure that lowercase c and uppercase C both work
 
-        switch (letter) {
+        switch (letter) {   // Determines which item the user has selected
             case 'A':
                 switch (number) {
-                    case '1': itemName = "Water"; price = 1.00; break;
-                    case '2': itemName = "Juice"; price = 1.50; break;
-                    case '3': itemName = "Cola"; price = 1.25; break;
-                    default: cout << "Invalid code.\n\n"; continue;
+                    case '1': itemName = "Water"; price = 0.99; break; // Makes sure that if water is selected it charges the correct price
+                    case '2': itemName = "Juice"; price = 1.49; break; // Makes sure that if juice is selected it charges the correct price
+                    case '3': itemName = "Cola"; price = 1.25; break; // Makes sure that if cola is selected it charges the correct price
+                    default: cout << "Invalid code, please try again.\n"; continue; // Makes sure that a valid number is selected for an item code starting with a A
                 }
                 break;
 
             case 'B':
                 switch (number) {
-                    case '1': itemName = "Crisps"; price = 1.75; break;
-                    case '2': itemName = "Gum"; price = 0.75; break;
-                    case '3': itemName = "Chocolate"; price = 2.00; break;
-                    default: cout << "Invalid code.\n\n"; continue;
+                    case '1': itemName = "Crisps"; price = 1.75; break; // Makes sure that if crisps is selected it charges the correct price
+                    case '2': itemName = "Gum"; price = 0.75; break; // Makes sure that if gum is selected it charges the correct price
+                    case '3': itemName = "Chocolate"; price = 1.99; break; // Makes sure that if chocolate is selected it charges the correct price
+                    default: cout << "Invalid code, please try again.\n"; continue; // Makes sure that a valid number is selected for an item code starting with a B
                 }
                 break;
 
             case 'C':
                 switch (number) {
-                    case '1': itemName = "Mints"; price = 0.80; break;
-                    case '2': itemName = "Gummies"; price = 1.60; break;
-                    case '3': itemName = "Biscuits"; price = 1.40; break;
-                    default: cout << "Invalid code.\n\n"; continue;
+                    case '1': itemName = "Mints"; price = 0.79; break; // Makes sure that if mints is selected it charges the correct price
+                    case '2': itemName = "Gummies"; price = 1.59; break; // Makes sure that if gummies is selected it charges the correct price
+                    case '3': itemName = "Biscuits"; price = 1.39; break; // Makes sure that if biscuits is selected it charges the correct price
+                    default: cout << "Invalid code, please try again.\n"; continue; // Makes sure that a valid number is selected for an item code starting with a C
                 }
                 break;
 
             default:
-                cout << "Invalid code.\n";
+                cout << "Invalid code, please try again.\n"; // Makes sure that a valid letter and number is selected for the item code
                 continue;
         }
 
-        cout << "You selected: " << itemName << " (£" << price << ")\n";
+        cout << "You selected: " << itemName << " (£" << price << ")\n"; // Displays the item selected and its price
 
         if (confirmItem(itemName)) return true;
 
-        cout << "Please try again.\n";
+        cout << "Please enter code again.\n"; // Asks the user to try again if they did not confirm their item selection
     }
 }
 
 bool confirmItem(const string &itemName) {
     char answer;
-    cout << "Is this correct? (Y/N): ";
+    cout << "Is this correct? (Y/N): "; // Asks the user to confirm their item selection
     cin >> answer;
 
-    if (answer == 'y' || answer == 'Y') return true;
+    if (answer == 'y' || answer == 'Y') return true; // Makes sure that both lowercase y and uppercase Y work to confirm the item selection
     return false;
 }
 
 void handlePayment(double price) {
-    double total = 0.0;
+    double total = 0.00; // Tracks the total amount inserted
     double money;
 
-    cout << "Please insert £" << price << endl;
+    cout << "Please enter the value of £ inserted" << price << endl; // Asks the user to insert money for their selected item
 
-    while (total < price) {
+    while (total < price) { // Loops until the total amount inserted is equal to or greater than the price of the item
         cout << "Insert value (£ remaining " << price - total << "): ";
         cin >> money;
 
-        if (cin.fail() || money <= 0) {
+        if (cin.fail() || money <= 0) { // Checks if the input is valid
             cin.clear();
-            cin.ignore(1000, '\n');
-            cout << "Invalid amount.\n";
+            cin.ignore(1000, '\n'); 
+            cout << "Sorry, Invalid amount.\n"; // Validates the money inserted
             continue;
         }
 
-        total += money;
+        total += money; // Adds the inserted money to the total amount
     }
 
     if (total > price) {
-        cout << "Change returned: £" << total - price << endl;
+        cout << "Change returned: £" << total - price << endl; // Returns change if the total amount inserted is greater than the price of the item
     }
 }
 
 bool askAnotherPurchase() {
     char choice;
-    cout << "Would you like to make another purchase? (Y/N): ";
+    cout << "Would you like to make another purchase? (Y/N): ";// Asks the user if they want to make another purchase
     cin >> choice;
     cout << endl;
 
-    if (choice == 'y' || choice == 'Y') return true;
+    if (choice == 'y' || choice == 'Y') return true; // Makes sure that both lowercase y and uppercase Y work to make another purchase
     return false;
 }
